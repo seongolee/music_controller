dc9e550c78df52fac8df37a0cee5d7c8589b7dd2
seongolee <seongo0521@gmail.com>
2022-05-28 17:55:24 +0900

    Feat: 업데이트 api 로직, 기능 추가

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, status
-from .serializers import RoomSerializer, CreateRoomSerializer
+from .serializers import RoomSerializer, CreateRoomSerializer, UpdateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -93,7 +93,6 @@ class CreateRoomView(APIView):
                 room_status = status.HTTP_201_CREATED
 
             self.request.session['room_code'] = room.code
-            print(self.request.session['room_code'])
             data = RoomSerializer(room).data
             data['is_host'] = True
 
@@ -101,6 +100,7 @@ class CreateRoomView(APIView):
             return Response(data, status=room_status)
 
 
+# 유저가 참여중인 방확인
 class UserInRoom(APIView):
     def get(self, request):
         if not self.request.session.exists(self.request.session.session_key):
@@ -110,3 +110,51 @@ class UserInRoom(APIView):
             'code': self.request.session.get('room_code')
         }
         return Response(data, status=status.HTTP_200_OK)
+
+
+# 방 나가기
+class LeaveRoom(APIView):
+    def post(self, request):
+        if 'room_code' in self.request.session:
+            code = self.request.session.pop('room_code')
+            host_id = self.request.session.session_key
+            room_results = Room.objects.filter(host=host_id)
+            if room_results:
+                room = room_results[0]
+                room.delete()
+
+        return Response({'Message': 'Success'}, status=status.HTTP_200_OK)
+
+
+#
+class UpdateRoom(APIView):
+    serializer_class = UpdateRoomSerializer
+
+    def patch(self, request):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data.get('votes_to_skip')
+            code = serializer.data.get('code')
+
+            queryset = Room.objects.filter(code=code)
+
+            # 방을 찾기 못함
+            if not queryset.exists():
+                return Response({'mgs': 'Room not found.'}, status=status.HTTP_404_NOT_FOUND)
+            else:
+                room = queryset[0]
+                user_id = self.request.session.session_key
+                if room.host != user_id:
+                    return Response({'msg': 'You are not the host of this room.'}, status=status.HTTP_403_FORBIDDEN)
+                else:
+                    room.guest_can_pause = guest_can_pause
+                    room.votes_to_skip = votes_to_skip
+                    room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                    return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+
+        # 유효하지 않은 데이터
+        return Response({'Bad Request': "Invalid Data..."}, status=status.HTTP_400_BAD_REQUEST)
--- music_frontend/src/pages/room/Room.js
+++ music_frontend/src/pages/room/Room.js
@@ -1,65 +1,119 @@
 import React, {useState, useLayoutEffect} from "react";
-import {useParams, useLocation, Link} from "react-router-dom";
-import axios from"axios";
+import {useParams, useLocation, useNavigate} from "react-router-dom";
+import axios from "axios";
 import {Grid, Button, Typography} from "@material-ui/core";
+import {CreateRoomPage} from "../index";
 
 const Room = () => {
     // Navigate state
     const location = useLocation();
+    const navigate = useNavigate();
 
     const [votesToSkip, setVotesToSkip] = useState(2);
     const [guestCanPause, setGuestCanPause] = useState(false);
     const [isHost, setIsHost] = useState(false);
+    const [showSettings, setShowSettings] = useState(false);
     const {roomCode} = useParams();
-
 
     const getRoomDetails = async () => {
         try {
             const response = await axios.get('/api/get-room?code=' + roomCode);
             const data = response.data;
-            console.log(data);
             setVotesToSkip(data.votes_to_skip);
-            setGuestCanPause(data.guest_can_pause);
-            setIsHost(data.is_host);
-        } catch(e) {
-            console.error(e)
+            setGuestCanPause(Boolean(data.guest_can_pause));
+            setIsHost(Boolean(data.is_host));
+        } catch (e) {
+            console.error(e);
+            navigate('/');
         }
+    }
+
+    const leaveButtonPressed = async () => {
+        try {
+            const response = await axios.post('/api/leave-room', {}, {
+                withCredentials: true
+            });
+            navigate('/');
+        } catch (e) {
+            console.log(e.response.data);
+        }
+    }
+
+    const updateShowSettings = (value) => {
+        setShowSettings(value)
+    }
+
+    const renderSettingsButton = () => {
+        return (
+            <Grid item xs={12} align="center">
+                <Button variant="contained" color="primary" onClick={() => updateShowSettings(true)}>
+                    Settings
+                </Button>
+            </Grid>
+        );
     }
 
     useLayoutEffect(() => {
         getRoomDetails();
     }, []);
 
-    return (
-        <Grid container spacing={1}>
-            <Grid item xs={12} align="center">
-                <Typography variant="h6" component="h6">
-                    Code: {roomCode}
-                </Typography>
+    const renderSettings = () => {
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <CreateRoomPage
+                        update={true}
+                        votesToSkip={votesToSkip}
+                        guestCanPause={guestCanPause}
+                        roomCode={roomCode}
+                        updateCallback={() => {
+                        }}
+                    >
+                    </CreateRoomPage>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="primary" onClick={() => updateShowSettings(false)}>
+                        Close
+                    </Button>
+                </Grid>
             </Grid>
-            <Grid item xs={12} align="center">
-                <Typography variant="h6" component="h6">
-                    Votes: {votesToSkip}
-                </Typography>
-            </Grid>
-            <Grid item xs={12} align="center">
-                <Typography variant="h6" component="h6">
-                    Guest Can Pause: {guestCanPause.toString()}
-                </Typography>
-            </Grid>
-            <Grid item xs={12} align="center">
-                <Typography variant="h6" component="h6">
-                    Host: {isHost.toString()}
-                </Typography>
-            </Grid>
-            <Grid item xs={12} align="center">
-                <Button variant="contained" color="secondary" to="/" component={Link}>
-                    Leave Room
-                </Button>
-            </Grid>
-        </Grid>
+        );
+    }
 
-    );
+    if (showSettings) {
+        return renderSettings();
+    } else {
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h6" component="h6">
+                        Code: {roomCode}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h6" component="h6">
+                        Votes: {votesToSkip}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h6" component="h6">
+                        Guest Can Pause: {guestCanPause.toString()}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h6" component="h6">
+                        Host: {isHost.toString()}
+                    </Typography>
+                </Grid>
+                {isHost ? renderSettingsButton() : null}
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="secondary" onClick={leaveButtonPressed}>
+                        Leave Room
+                    </Button>
+                </Grid>
+            </Grid>
+        );
+    }
 }
 
 export default Room;
